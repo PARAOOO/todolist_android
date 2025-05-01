3856ed930bb22cf946f2f9a2cc14f45bc1a8ad24
PARAOOO <nakim3159@gmail.com>
2025-05-01 18:12:31 +0900

    fix: change versionCode, versionName

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.paraooo.todolist"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.4.1"
+        versionCode = 8
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
