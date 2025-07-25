20ad83dd6de2fde68df984c31cd087320219174e
PARAOOO <nakim3159@gmail.com>
2025-07-25 14:24:12 +0900

    update: add Models copying Entity

--- domain/src/main/java/com/paraooo/domain/model/TodoModel.kt
+++ domain/src/main/java/com/paraooo/domain/model/TodoModel.kt
@@ -15,6 +15,7 @@ enum class AlarmType(val label : String) {
 
 data class TodoModel(
     val instanceId : Long,
+    val templateId : Long,
     val title : String,
     val time : Time? = null,
     val date: LocalDate,
@@ -29,3 +30,54 @@ data class TodoModel(
     val isAlarmHasVibration : Boolean = false,
     val isAlarmHasSound : Boolean = false
 )
+
+data class TodoTemplateModel(
+    val id: Long = 0,
+    val title: String,
+    val description: String,
+    val hour: Int?, // null이면 시간 미지정,
+    val minute: Int?,
+    val type: TodoType, // GENERAL, PERIOD, DAY_OF_WEEK,
+    val alarmType : AlarmType, // OFF, NOTIFY, POPUP
+    val isAlarmHasVibration : Boolean,
+    val isAlarmHasSound : Boolean
+)
+
+data class TodoInstanceModel(
+    val id: Long = 0,
+    val templateId: Long,
+    val date: Long,
+    val progressAngle: Float = 0F,
+)
+
+data class TodoPeriodModel(
+    val templateId: Long,
+    val startDate: Long,
+    val endDate: Long
+)
+
+data class TodoDayOfWeekModel(
+    val id: Long = 0,
+    val templateId: Long,
+    val dayOfWeeks: List<Int>,
+    val dayOfWeek: Int
+)
+
+data class TodoPeriodWithTimeModel(
+    val templateId : Long,
+    val hour: Int?,
+    val minute: Int?,
+    val startDate: Long,
+    val endDate: Long
+)
+
+data class TodoDayOfWeekWithTimeModel(
+    val templateId : Long,
+    val hour: Int?,
+    val minute: Int?,
+    val dayOfWeeks : List<Int>
+)
+
+enum class TodoType {
+    GENERAL, PERIOD, DAY_OF_WEEK
+}
