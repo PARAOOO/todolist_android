a2cafe5b4f1cc69353a8e6cc33486c6cb59c17f2
PARAOOO <nakim3159@gmail.com>
2025-08-11 06:26:40 +0900

    refactor: separate data orchestration logic and business logic into repository and usecase (15%)

--- domain/src/main/java/com/paraooo/domain/usecase/todo/DeleteTodoByIdUseCase.kt
+++ domain/src/main/java/com/paraooo/domain/usecase/todo/DeleteTodoByIdUseCase.kt
@@ -2,19 +2,30 @@ package com.paraooo.domain.usecase.todo
 
 import com.paraooo.domain.repository.AlarmScheduler
 import com.paraooo.domain.repository.TodoInstanceRepository
+import com.paraooo.domain.repository.TodoRepository
 import com.paraooo.domain.repository.TodoTemplateRepository
 
 class DeleteTodoByIdUseCase(
+    private val todoRepository: TodoRepository,
     private val todoInstanceRepository: TodoInstanceRepository,
     private val todoTemplateRepository: TodoTemplateRepository,
     private val alarmScheduler: AlarmScheduler
 ) {
 
     suspend operator fun invoke(instanceId: Long) {
-        val instanceTodo = todoInstanceRepository.getTodoInstanceById(instanceId)
 
-        todoTemplateRepository.deleteTodoTemplate(instanceTodo!!.templateId)
+        val instanceTodo = todoRepository.getTodoInstanceByInstanceId(instanceId) ?: return
 
-        alarmScheduler.cancel(templateId = instanceTodo.templateId)
+//        val instanceTodo = todoInstanceRepository.getTodoInstanceById(instanceId)
+
+//        todoTemplateRepository.deleteTodoTemplate(instanceTodo!!.templateId)
+
+        try {
+            todoRepository.deleteTodoTemplate(instanceTodo.templateId)
+            alarmScheduler.cancel(templateId = instanceTodo.templateId)
+        } catch (e : Exception) {
+
+        }
+
     }
 }
--- domain/src/main/java/com/paraooo/domain/usecase/todo/UpdateTodoProgressUseCase.kt
+++ domain/src/main/java/com/paraooo/domain/usecase/todo/UpdateTodoProgressUseCase.kt
@@ -1,12 +1,14 @@
 package com.paraooo.domain.usecase.todo
 
 import com.paraooo.domain.repository.TodoInstanceRepository
+import com.paraooo.domain.repository.TodoRepository
 
 class UpdateTodoProgressUseCase(
-    private val todoInstanceRepository: TodoInstanceRepository
+//    private val todoInstanceRepository: TodoInstanceRepository,
+    private val todoRepository: TodoRepository
 ) {
 
     suspend operator fun invoke(instanceId: Long, progress: Float) {
-        todoInstanceRepository.updateTodoProgress(instanceId, progress)
+        todoRepository.updateTodoProgress(instanceId, progress)
     }
 }
