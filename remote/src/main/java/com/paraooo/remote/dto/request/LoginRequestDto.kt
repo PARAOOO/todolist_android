18777d0eb1a2c46c2fa6f4e9cd31b9138baff555
paraooo <nakim3159@gmail.com>
2025-08-28 17:44:13 +0900

    update: update network logic (20%)

---
+++ remote/src/main/java/com/paraooo/remote/dto/request/LoginRequestDto.kt
@@ -0,0 +1,6 @@
+package com.paraooo.remote.dto.request
+
+data class LoginRequestDto(
+    val email: String,
+    val password: String
+)
---
+++ remote/src/main/java/com/paraooo/remote/dto/request/SendVerificationCodeRequestDto.kt
@@ -0,0 +1,5 @@
+package com.paraooo.remote.dto.request
+
+data class SendVerificationCodeRequestDto(
+    val email: String
+)
